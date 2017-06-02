02277b17b582c4edfc63f956fc447ec5e1449996
Sam Elsamman <sam.elsamman@gmail.com>
2017-06-02 16:33:42 -0400

    Got rid of static

--- apps/batch/js/controller.ts
+++ apps/batch/js/controller.ts
@@ -1,7 +1,6 @@
 import {Supertype, supertypeClass, Persistable, property, remote, amorphicStatic, Bindable} from 'amorphic';
 import { TicketItemComment } from '../../common/js/ticketItemComment';
 import {Ticket} from '../../common/js/ticket';
-import {List} from '../../common/js/static';
 import * as Q from 'Q';
 import * as _ from 'underscore';
 import {Person} from "../../common/js/person";
@@ -13,7 +12,6 @@ var forceImport = TicketItemComment;
 export class Controller extends Persistable(Supertype) {
     serverInit () {
         amorphicStatic.logger.info({userConfigFromAmorphic: this.amorphic.config.userConfig, userConfigFromStatic: amorphicStatic.config.userConfig}, 'Damon Up and Running ');
-        this.amorphic.logger.info({templateName: List.template.amorphicClassName}, 'Look ma a static');
         setTimeout(() => this.tickTock(), 5000);
 
     }
--- apps/common/js/static.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var project_1 = require("./project");
-exports.List = {
-    template: project_1.Project
-};
-//# sourceMappingURL=static.js.map
