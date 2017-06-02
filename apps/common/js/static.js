9ba6445f5841c70f5d6e81b68ad54d9144fdd60d
Sam Elsamman <sam.elsamman@gmail.com>
2017-06-02 16:27:19 -0400

    Somehow forgot to check in static.js

---
+++ apps/common/js/static.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var project_1 = require("./project");
+exports.List = {
+    template: project_1.Project
+};
+//# sourceMappingURL=static.js.map
